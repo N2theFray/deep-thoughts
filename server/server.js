6c162bdd46ce1753962a23f2fa66836e6455fa1d
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-07-05 09:59:59 -0400

    added resolvers

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+//import ApolloServer
+const { ApolloServer } = require('apollo-server-express')
+const { authMiddleware } = require('./utils/auth')
+
+//import our typeDefs and resolvers
+const { typeDefs, resolvers } =require('./schemas')
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//create a new Apollo server and pass in our schema data
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+//integrate our Apollo server with th Express application as middleware
+server.applyMiddleware({ app })
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    //log where we can go test our GQL APi
+    console.log(`Use GraphQl to http://localhost:${PORT}${server.graphqlPath}`)
+  });
+});
